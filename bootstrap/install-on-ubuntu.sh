2d10ec73949f4db21fe58ae59a28598424936b6b
Yuta Takahashi <y.highbridge04@gmail.com>
2023-09-26 13:17:40 +0900

    update

--- bootstrap/install-on-ubuntu.sh
+++ bootstrap/install-on-ubuntu.sh
@@ -1,41 +1,53 @@
 #! /bin/bash
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
 
-tee /etc/modules-load.d/containerd.conf <<EOF
+apt-get install -y iptables arptables ebtables
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+
+cat > /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
 
 modprobe overlay
-modprove br_netfilter
+modprobe br_netfilter
 
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
 EOF
 
 sysctl --system
 
-apt-get update
-apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+apt-get update && apt-get install -y containerd
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt-get update
-sudo apt-get install containerd.io -y
-
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+if grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
+  echo "Config found, skip rewriting..."
+else
+  sed -i -e "s/SystemdCgroup \= false/SystemdCgroup \= true/g" /etc/containerd/config.toml
+fi
 
 systemctl restart containerd
 systemctl enable containerd
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-
+apt-get update && apt-get install -y apt-transport-https curl gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
+systemctl enable kubelet
